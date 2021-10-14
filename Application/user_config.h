ca3bb6c78d0e1121c627cf75d21b908ffcf5f267
Abhimanyu K K <abhimanyukk01@gmail.com>
2021-10-14 11:33:07 +0000

    Update Application/user_config.h

--- Application/user_config.h
+++ Application/user_config.h
@@ -9,14 +9,4 @@
 #define FUNCTION_CODE       1
 #define STARTING_ADDRESS    0x1001
 
-// #define COIL_ADDRESS        {0x1000, 0x1001, 0x1002, 0x1003, 0x1004, 0x1005, 0x1006, 0x1007, 0x1008, 0x1009, 0x100A, 0x100B, 0x100C, 0x100D, 0x100E, 0x100F}
-// #define COIL_ADDRESS        "aaa"
-// #define COIL_STATUS         {true, false, true, true, false, true, true, false, true, true, false, true, true, false, false, true}
-
-// typedef struct __attribute__((packed, scalar_storage_order("big-endian"))) DataBase
-// {
-//     uint16_t *coil_address;
-//     uint8_t *coil_status;
-// }dataBase_t;
-
 #endif // USER_CONFIG_H
